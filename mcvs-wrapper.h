ea330f123c986f7d252468b2b903c014d29512f5
ggkitsas <ggiktsas@yahoo.com>
2014-11-19 11:01:10 +0100

    sec logic at linux functs, fixed 'sizeof-at-macro' bug

--- mcvs-wrapper.h
+++ mcvs-wrapper.h
@@ -2,46 +2,52 @@
 #define MCVS_H
 
 
-#define STRNCPY ( (dest) (dest_size) (source) (source_size) )   \
-#ifdef _MSC_VER                                                 \
-strncpy_s( (dest), (dest_size), (source), (source_size) );      \
-#else                                                           \
-strcpy ((dest), (source), (source_size) );                      \
-#endif
+#ifdef _MSC_VER
+
+    #define STRNCPY(dest,dest_size,source,source_size)          strncpy_s( (dest), (dest_size), (source), (source_size) );
+    #define STRCAT(dest,dest_size,source)                       strcat_s ( (dest) , (dest_size), (source) );
+    #define SNPRINTF( dest, dest_size, format, ...)             _snprintf_s ( (dest), (dest_size), (format) , ##__VA_ARGS__);
 
 
-#define STRCAT( (dest) , (dest_size) , (source) )   \
-#ifdef _MSC_VER                                     \
-strcat_s ( (dest) , (dest_size), (source) );        \
-#else                                               \
-do{
-if ( strlen(DebugNewText) > sizeof(DebugText_Stick20) - strlen(DebugText_Stick20) -1 )
-                return;
-strcat (DebugText_Stick20, DebugNewText);                                          \
-}while(0);
+    #define FOPEN(FP, file, options)                    \
+    do {                                                \
+        errno_t Err_t;                                  \
+        Err_t = fopen_s ( (FP) , (file), (options));    \
+        (FP) = 0;                                       \
+    }while(0);
+
+#else
+
+    #define STRNCPY(dest,dest_size,source,source_size)  \
+    do{                                                 \
+        if ( (source_size) < (dest_size)) {             \
+            strncpy ((dest), (source), (source_size) ); \
+            (dest)[ (source_size) ] = '\0';             \
+        } else {                                        \
+            (void)0;                                    \
+        }                                               \
+    }while(0);
 
 
-#endif
-
-#define SNPRINTF( (dest) , (dest_size) , (format), ...)                     \
-#ifdef _MSC_VER                                                             \
-_snprintf_s ( (dest), (dest_size), (format) , __VA_ARGS__);     \
-#else                                                                       \
-snprintf ( (dest), (dest_size), (format) ,  __VA_ARGS__);      \
-#endif
-
-#define FOPEN( fp, file, options)                   \
-do {                                                \
-    #ifdef _MSC_VER                                 \
-    errno_t Err_t;                                  \
-    Err_t = fopen_s ( (fp) , (file), (options));    \
-    (fp) = 0;                                       \
-    #else                                           \
-    (fp) = fopen ((file), (options));               \
-    if (NULL == (fp))                               \
-        (fp) = 0;                                   \
-    #endif                                          \
-}while(0);
+    #define STRCAT(dest,dest_size,source)                                           \
+    do{                                                                             \
+        if ( strlen((source)) <= (dest_size) - strlen((dest)) -1 ){                 \
+            printf("%d %d %d\n", strlen((source)), sizeof((dest)), strlen((dest))); \
+            strcat ((dest), (source));                                              \
+        } else {                                                                    \
+            (void)0;                                                                \
+        }                                                                           \
+    }while(0);
 
 
-#endif /* MCVS_H */
+    #define SNPRINTF( dest , dest_size , format, ...)     snprintf ( (dest), (dest_size), (format) ,  ##__VA_ARGS__);
+
+
+    #define FOPEN(FP, file, options)                    \
+    do{                                                 \
+        (FP) = fopen ((file), (options));               \
+        if (NULL == (FP))                               \
+            (FP) = 0;                                   \
+    }while(0);
+
+#endif /* _MSC_VER */
