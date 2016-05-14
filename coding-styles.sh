840525c5dc11242fda6361d3e81135c3447f72ff
Szczepan Zalega <szczepan@nitrokey.com>
2016-05-14 13:04:23 +0200

    Execute clang-format with only 1 file at a time.
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- coding-styles.sh
+++ coding-styles.sh
@@ -3,8 +3,8 @@
 #style is defined in .clang-format
 
 # run for all files
-#cat <(find . -name '*.cpp') <(find . -name '*.h') | xargs clang-format-3.8 -i
+#cat <(find . -name '*.cpp') <(find . -name '*.h') | xargs -n1 clang-format-3.8 -i
 
-#run only on lately changed
+#run only on not committed files
 git status | sed 's|new file|new_file|g' | grep -e modified -e new_file | awk '{print $2}' | xargs clang-format-3.8 -i
 
