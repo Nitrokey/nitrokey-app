ff559d869b6616149b8e828ec3d975af7ba8ecee
Szczepan Zalega <szczepan@nitrokey.com>
2016-05-14 13:05:42 +0200

    As previous commit but for just changed
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- coding-styles.sh
+++ coding-styles.sh
@@ -6,5 +6,5 @@
 #cat <(find . -name '*.cpp') <(find . -name '*.h') | xargs -n1 clang-format-3.8 -i
 
 #run only on not committed files
-git status | sed 's|new file|new_file|g' | grep -e modified -e new_file | awk '{print $2}' | xargs clang-format-3.8 -i
+git status | sed 's|new file|new_file|g' | grep -e modified -e new_file | awk '{print $2}' | xargs -n1 clang-format-3.8 -i
 
