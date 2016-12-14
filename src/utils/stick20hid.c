a0fdf29594209e94675e0c7ba4d0cb2ec2837ad7
Szczepan Zalega <szczepan@nitrokey.com>
2016-12-14 19:33:17 +0100

    Save compilation information to debug log
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- src/utils/stick20hid.c
+++ src/utils/stick20hid.c
@@ -92,6 +92,18 @@ int Stick20_ProductionInfosChanged = FALSE;
 
 typeStick20ProductionInfos_st Stick20ProductionInfos_st;
 
+#ifndef CMAKE_BUILD_TYPE
+#define CMAKE_BUILD_TYPE ""
+#endif
+#ifndef CMAKE_CXX_COMPILER
+#define CMAKE_CXX_COMPILER ""
+#endif
+#ifndef CMAKE_CXX_FLAGS
+#define CMAKE_CXX_FLAGS ""
+#endif
+#ifndef HAVE_LIBAPPINDICATOR
+#define HAVE_LIBAPPINDICATOR "NO"
+#endif
 
 /* forward declaration */
 void DebugAppendFileGuiText(const char *Text);
@@ -107,6 +119,11 @@ void initDebugging (void)
 
     STRCPY (DebugingStickFilename, sizeof (DebugingStickFilename), "Firmwarelog.txt");
     STRCPY (DebugingGuiFilename, sizeof (DebugingGuiFilename), "Guilog.txt");
+
+    DebugAppendTextGui ("CMAKE_BUILD_TYPE " CMAKE_BUILD_TYPE "\n");
+    DebugAppendTextGui ("CMAKE_CXX_COMPILER " CMAKE_CXX_COMPILER "\n");
+    DebugAppendTextGui ("CMAKE_CXX_FLAGS " CMAKE_CXX_FLAGS "\n");
+    DebugAppendTextGui ("HAVE_LIBAPPINDICATOR: " HAVE_LIBAPPINDICATOR "\n");
 
     DebugAppendTextGui ((char *) "Start Debug - ");
 #ifdef WIN32
