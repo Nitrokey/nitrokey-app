18ae733eaf3e520ccadbed60ac49cb529506fadb
Szczepan Zalega <szczepan@nitrokey.com>
2017-02-15 13:21:55 +0100

    Handle case when CMAKE is not used
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- src/version.h
+++ src/version.h
@@ -1,6 +1,12 @@
 #ifndef NITROKEYAPP_VERSION_H
 #define NITROKEYAPP_VERSION_H
 
+#ifndef CMAKE_BUILD_TYPE
+#define CMAKE_BUILD_TYPE "---"
+#define CMAKE_CXX_COMPILER "---"
+#define CMAKE_CXX_FLAGS "---"
+#endif
+
 #ifndef GUI_VERSION
 #define GUI_VERSION "1.0"
 #endif
