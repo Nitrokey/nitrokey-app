34a4409b7b353144257b482c414237d6a06d83c3
Szczepan Zalega <szczepan@nitrokey.com>
2017-04-11 17:12:31 +0200

    Bump version to beta.3
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- src/version.h
+++ src/version.h
@@ -2,17 +2,17 @@
 #define NITROKEYAPP_VERSION_H
 
 #ifndef CMAKE_BUILD_TYPE
-#define CMAKE_BUILD_TYPE "-"
-#define CMAKE_CXX_COMPILER "-"
-#define CMAKE_CXX_FLAGS "-"
+#define CMAKE_BUILD_TYPE ""
+#define CMAKE_CXX_COMPILER ""
+#define CMAKE_CXX_FLAGS ""
 #endif
 
 #ifndef GUI_VERSION
-#define GUI_VERSION "1.0-beta.2"
+#define GUI_VERSION "1.0-beta.3"
 #endif
 
 #ifndef GIT_VERSION
-#define GIT_VERSION "-"
+#define GIT_VERSION ""
 #endif
 
 #endif //NITROKEYAPP_VERSION_H
