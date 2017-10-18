21bf29bd8dc0c929af63b3bbf559b1f8384fbf6f
Szczepan Zalega <szczepan@nitrokey.com>
2017-10-18 10:22:39 +0200

    update
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- support/appveyor-build.py
+++ support/appveyor-build.py
@@ -12,13 +12,13 @@ cmake_command = ['cmake', '-DFMT_PEDANTIC=ON', '-DCMAKE_BUILD_TYPE=' + config]
         
 if build == 'mingw':
     cmake_command.append('-GMinGW Makefiles')
-    cmake_command.append(r'-DCMAKE_USER_MOD_PATH=C:\\Qt\\5.9.1\\mingw53_32\\lib\\cmake')
+    cmake_command.append(r'-DCMAKE_USER_MOD_PATH=C:/Qt/5.9.1/mingw53_32/lib/cmake')
     build_command = ['mingw32-make', '-j2']
     test_command = ['mingw32-make', 'test']
     # Remove the path to Git bin directory from $PATH because it breaks
     # MinGW config.
-    path = path.replace(r'C:\Program Files (x86)\Git\bin', '')
-    os.environ['PATH'] = r'C:\MinGW\bin;' + path
+    path = path.replace(r'C:/Program Files (x86)/Git/bin', '')
+    #os.environ['PATH'] = r'C:/MinGW/bin;' + path
 elif build == 'qmake':
     cmake_command = ['qmake', '.']
     build_command = ['mingw32-make', '-j2']
@@ -26,12 +26,12 @@ elif build == 'qmake':
 else:
     # Add MSBuild 14.0 to PATH as described in
     # http://help.appveyor.com/discussions/problems/2229-v140-not-found-on-vs2105rc.
-    os.environ['PATH'] = r'C:\Program Files (x86)\MSBuild\14.0\Bin;' + path
+    os.environ['PATH'] = r'C:/Program Files (x86)/MSBuild/14.0/Bin;' + path
     generator = 'Visual Studio 14 2015'
     if platform == 'x64':
         generator += ' Win64'
     cmake_command.append('-G' + generator)
-    cmake_command.append(r'-DCMAKE_USER_MOD_PATH=C:\\Qt\\5.9.1\\msvc2015\\lib\\cmake')
+    cmake_command.append(r'-DCMAKE_USER_MOD_PATH=C:/Qt/5.9.1/msvc2015/lib/cmake')
     build_command = ['cmake', '--build', '.', '--config', config, '--', '/m:2']
     test_command = ['ctest', '-C', config]
 
