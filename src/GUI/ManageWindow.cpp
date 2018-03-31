4c3d7562f580bcdd8632a13407b63a1681b32d7d
Szczepan Zalega <szczepan@nitrokey.com>
2018-03-31 07:57:55 +0200

    Another method for window activation
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- src/GUI/ManageWindow.cpp
+++ src/GUI/ManageWindow.cpp
@@ -26,10 +26,20 @@
 #include "ManageWindow.h"
 
 void ManageWindow::bringToFocus(QWidget *w) {
+
+//    for ( QWindow* appWindow : qApp->allWindows() )
+//    {
+//      appWindow->show(); //bring window to top on OSX
+//      appWindow->raise(); //bring window from minimized state on OSX
+
+//      appWindow->requestActivate(); //bring window to front/unminimize on windows
+//    }
+
   w->setWindowState( (w->windowState() & ~Qt::WindowState::WindowMinimized) | Qt::WindowActive);
   w->show();
   w->raise();
   w->activateWindow();
+  w->raise();
 }
 
 #include <QDesktopWidget>
