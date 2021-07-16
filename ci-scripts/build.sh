9636028a5904570c3f2bff1ddf6e4f6bbe1de392
lennard.boediger <lennard@nitrokey.com>
2021-07-17 01:02:17 +0200

    ci

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -3,6 +3,10 @@ set -exuo pipefail
 export 
 
 . ./nitrokey-app-source-metadata/metadata
+ls
+cd output
+ls
+cd ..
 tar xf ./output/nitrokey-app-${NITROKEY_APP_BUILD_ARTIFACT_VERSION}.tar.gz
 
 pushd nitrokey-app-${NITROKEY_APP_BUILD_ARTIFACT_VERSION}
